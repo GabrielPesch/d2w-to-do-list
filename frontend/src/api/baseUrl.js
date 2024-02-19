4b3cf29b52871903b545d77ed98b233d98817993
GabrielPesch <gkpesch@gmail.com>
2024-02-19 15:49:27 -0300

    feat(frontend): Create axios instance

---
+++ frontend/src/api/baseUrl.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const accessToken = localStorage.getItem("access_token");
+
+const baseUrl = axios.create({
+  baseURL: "http://127.0.0.1:5000/",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
+
+export const instancedBaseUrl = (token) => {
+  return axios.create({
+    baseURL: "http://127.0.0.1:5000/",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export default baseUrl;
