da5e59a77c8263a12bf38cbd58fdc7975c55c9f5
GabrielPesch <gkpesch@gmail.com>
2024-02-19 15:41:01 -0300

    chore(backend): Update config for docker

--- backend/app/extensions/configuration.py
+++ backend/app/extensions/configuration.py
@@ -6,16 +6,16 @@ load_dotenv()
 
 def init_app(app):
     # para executar sem o docker
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
-        f"@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/?client_encoding=utf8"
-    )
-
-    # para executar com o docker
     # app.config["SQLALCHEMY_DATABASE_URI"] = (
     #     f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
-    #     f"@db:{os.environ['POSTGRES_PORT']}"
+    #     f"@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/?client_encoding=utf8"
     # )
+
+    # para executar com o docker
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
+        f"@db:{os.environ['POSTGRES_PORT']}"
+    )
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
