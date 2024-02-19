b37334156f72b0253992737fd9c0325b3a3ee13f
GabrielPesch <gkpesch@gmail.com>
2024-02-19 15:36:02 -0300

    chore(backend): create Date time helper

---
+++ backend/app/blueprints/restapi/helpers/time.py
@@ -0,0 +1,8 @@
+import pytz
+from datetime import datetime
+
+
+def time_now():
+    saopaulo_tz = pytz.timezone("America/Sao_Paulo")
+    utc_now = datetime.utcnow().replace(tzinfo=pytz.utc)
+    return utc_now.astimezone(saopaulo_tz)
