8e7b802b8f840e02bffa1cfdbbd206c68bb9d36b
GabrielPesch <gkpesch@gmail.com>
2024-02-19 15:52:43 -0300

    feat(frontend): Create home page

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+import AppContent from "../components/Content/Content";
+import AppLayout from "../components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <AppLayout>
+      <AppContent />
+    </AppLayout>
+  );
+}
